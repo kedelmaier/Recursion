7125b5345dbe5be25f0876f6428d6a9c5348f381
kedelmaier <70293112+kedelmaier@users.noreply.github.com>
2020-11-10 00:56:31 -0600

    Add files via upload

--- EatCake.java
+++ EatCake.java
@@ -1,1 +1,18 @@
+public class EatCake {
+	
+	public EatCake() {
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		Cake cake = new Cake();
+		eatCake(cake);	
+	}
 
+	private static void eatCake(Cake cake) {
+		
+	}
+	
+	
+}
