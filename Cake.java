30115ebcede2eee69bb553f5233ff8e455880916
kedelmaier <70293112+kedelmaier@users.noreply.github.com>
2020-11-10 00:54:53 -0600

    Add files via upload

--- Cake.java
+++ Cake.java
@@ -1,1 +1,19 @@
-
+public class Cake {
+	
+	private int bites = 10;
+	
+	public Cake() {
+		//nothing to implement
+	}
+	
+	public void takeABite() {
+		--bites;
+		System.out.println("Hmm.. ");
+	}
+	
+	public boolean isEmpty() {
+		
+		return false;
+	}
+	
+}
